505ab86d1a5e746ead925fe1c2450f2d5b28db4c
5-23 dev <86705803+5-23@users.noreply.github.com>
2023-03-30 15:25:43 +0900

    make pluls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{process::Command, ops::Index};
 
 use regex::Regex;
 
@@ -13,29 +13,55 @@ async fn main() {
 
 fn han(codes: String) -> String{
     let mut end_code = String::new();
+    let re_rem = Regex::new(r"주석 (?P<code>.*)").unwrap();
     let re_print = Regex::new(r"출력\((?P<code>.*)\)").unwrap();
     let re_input = Regex::new(r"입력\((?P<code>.*)\)").unwrap();
-    let re_var = Regex::new(r"변수\((?P<code>.*)\)").unwrap();
-    let re_rem = Regex::new(r"주석 (?P<code>.*)").unwrap();
+    // let re_use_var = Regex::new(r"변수\((?P<code>.*)\)").unwrap();
+    let re_use_var2 = Regex::new(r"(?P<code1>.*)변수\((?P<code2>.*)\)(?P<code3>.*)").unwrap();
+    let re_set_var1 = Regex::new(r"(?P<name>.*)(.*)는(?P<code>.*)").unwrap();
+    let re_set_var2 = Regex::new(r"(?P<name>.*)(.*)은(?P<code>.*)").unwrap();
+    let re_operator_plus = Regex::new(r"(?P<var>.*)\+=(?P<code>.*)").unwrap();
 
     for code in codes.split(";"){
-        if let Option::Some(_) = re_rem.captures(code){
+        let mut code = code.to_string();
+
+        if let Option::Some(_) = re_rem.captures(&code){ continue }
+
+        if let Option::Some(a) = re_operator_plus.captures(&code){
+            let s = (&a["code"]).to_string();
+            end_code.push_str(format!("set /a {}+={}\n", &a["var"].replace(" ", ""), han(s)).as_str());
+            code = "".to_string();
+        }
+
+        if let Option::Some(a) = re_use_var2.captures(&code){
+            code = format!("{}%{}%{}", &a["code1"], han((&a["code2"]).to_string()), han((&a["code3"]).to_string()));
+            println!("code: {code}");
+            // println!("code1: {}", &a["code1"]);
+            // println!("code2: {}", &a["code2"]);
+            // println!("code3: {}", &a["code3"]);
+        }
+
+
+        
+        if let Option::Some(a) = re_print.captures(&code){
+            end_code.push_str(format!("echo {}\n", &a["code"]).as_str());
         }
         
-        else if let Option::Some(a) = re_print.captures(code){
-            end_code.push_str(format!("echo {}\n", han((&a["code"]).to_string())).as_str());
-        }
-        
-        else if let Option::Some(a) = re_input.captures(code){
+        else if let Option::Some(a) = re_input.captures(&code){
             end_code.push_str(format!("set /p \"{}=>\"\n", han((&a["code"]).to_string())).as_str());
         }
-        
-        else if let Option::Some(a) = re_var.captures(code){
-            end_code.push_str(format!("%{}%", han((&a["code"]).to_string())).as_str());
+
+        else if let Option::Some(a) = re_set_var1.captures(&code){
+            let s = (&a["code"]).to_string();
+            end_code.push_str(format!("set /a {}={}\n", &a["name"].replace(" ", ""), han(if &s[0..1] == " "{(&s[1..]).to_string()}else {s})).as_str());
+        }
+        else if let Option::Some(a) = re_set_var2.captures(&code){
+            let s = (&a["code"]).to_string();
+            end_code.push_str(format!("set /a {}={}\n", &a["name"].replace(" ", ""), han(if &s[0..1] == " "{(&s[1..]).to_string()}else {s})).as_str());
         }
         
         else{
-            end_code.push_str(code)
+            end_code.push_str(&code)
         }
         
         // if let Option::Some(a) = re_print.captures(code){
--- 완료.bat
+++ 완료.bat
@@ -1,9 +1,6 @@
 @echo off
-echo input : 
-set /p "a=>"
-echo %a%
-echo input : 
-set /p "a=>"
-echo %a%
+set /a e=1
+set /a e=%e%+1
+echo %e
 
 pause
